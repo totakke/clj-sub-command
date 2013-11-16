53d8eb39abcc6a11ec47b9617f7419d2d3da31a5
Toshiki TAKEUCHI <roimisia@gmail.com>
2013-11-16 19:14:13 +0900

    Fix test case

--- test/clj_sub_command/t_core.clj
+++ test/clj_sub_command/t_core.clj
@@ -2,48 +2,16 @@
   (:use midje.sweet
         clj-sub-command.core))
 
-(defn plus [& args]
-  (reduce + (map #(Integer/parseInt %) args)))
+(let [[opts cmd args _] (sub-command ["-p" "8080" "--no-verbose" "command1" "filename"]
+                                     :options  [["-p" "--port" :parse-fn #(Integer. %)]
+                                                ["--[no-]verbose" :default true]]
+                                     :commands [["command1" "desc for command1"]
+                                                ["command2" "desc for command2"]])]
+     (fact "Test for option parsing"
+       opts => {:port 8080, :verbose false})
 
-(defn prod [& args]
-  (reduce * (map #(Integer/parseInt %) args)))
+     (fact "Test for command parsing"
+       cmd => :command1)
 
-(defn do-sub-command-test [& args]
-  (do-sub-command args
-    "Test for do-sub-command macro"
-    [:sub1 plus "Plus args"]
-    [:sub2 prod]
-    [:sub3 :sub4 prod "Multiply args"]))
-
-(fact "about do-sub-command macro"
-  (do-sub-command-test "sub1" "2" "3") => 5
-  (do-sub-command-test "sub2" "2" "3") => 6
-  (do-sub-command-test "sub3" "3" "4") => 12
-  (do-sub-command-test "sub4" "3" "4") => 12
-  (do-sub-command-test "sub5" "3" "4") => (throws Exception)
-  (do-sub-command-test "-h") => nil?
-  (do-sub-command-test "-h" "sub1") => nil?)
-
-(defn with-sub-command-test [& args]
-  (with-sub-command args
-    "Test for with-sub-command macro"
-    [[opt?]
-     [[sub args] [[:sub1 "Plus args"]
-                  :sub2
-                  [:sub3 :sub4 "Multiply args"]]]]
-    (if opt?
-      "opt"
-      (condp = sub
-        :sub1 (apply plus args)
-        :sub2 (apply prod args)
-        :sub3 (apply prod args)))))
-
-(fact "about with-sub-command macro"
-  (with-sub-command-test "sub1" "2" "3") => 5
-  (with-sub-command-test "sub2" "2" "3") => 6
-  (with-sub-command-test "sub3" "3" "4") => 12
-  (with-sub-command-test "sub4" "3" "4") => 12
-  (with-sub-command-test "sub5" "3" "4") => (throws Exception)
-  (with-sub-command-test "--opt" "sub1") => "opt"
-  (with-sub-command-test "-h") => nil?
-  (with-sub-command-test "-h" "sub1") => nil?)
+     (fact "Test for sub-command arguments"
+       args => ["filename"]))
