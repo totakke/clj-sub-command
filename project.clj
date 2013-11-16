ab445f6b9f78d18405a34a99c3a3c418d5bcf9ce
Toshiki TAKEUCHI <roimisia@gmail.com>
2013-11-16 19:43:51 +0900

    Fix

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]
+                   :dependencies [[midje "1.5.1"]]}}
   :signing {:gpg-key "roimisia@gmail.com"})
