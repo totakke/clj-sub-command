02f317fc918633851b2171e01e853c60da6820a2
Toshiki TAKEUCHI <roimisia@gmail.com>
2013-11-16 19:52:12 +0900

    Bump up version to 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-sub-command "0.1.1-SNAPSHOT"
+(defproject clj-sub-command "0.2.0-SNAPSHOT"
   :description "A simple sub-command parser for Clojure."
   :url "https://github.com/totakke/clj-sub-command"
   :license {:name "Eclipse Public License"
