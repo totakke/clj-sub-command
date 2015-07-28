9960a093daa9df2b5c65c7596c5f0a9e0c151815
Toshiki Takeuchi <roimisia@gmail.com>
2015-07-28 15:47:39 +0900

    Bump up clojure to v1.7

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/totakke/clj-sub-command"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :profiles {:dev {:plugins [[lein-midje "3.1.3"]]
-                   :dependencies [[midje "1.6.3"]]}})
+                   :dependencies [[midje "1.7.0"]]}
+             :1.6 [:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}]})
