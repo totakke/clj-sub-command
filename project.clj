21e106e037fd98423d6c76a4ba7892cddc833221
Toshiki Takeuchi <roimisia@gmail.com>
2016-02-07 20:07:16 +0900

    Bump Midje up to version 1.8.3

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.3"]]
-                   :dependencies [[midje "1.7.0"]]}
+  :profiles {:dev {:plugins [[lein-midje "3.2"]]
+                   :dependencies [[midje "1.8.3"]]}
              :1.6 [:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}]}
   :signing {:gpg-key "roimisia@gmail.com"})
