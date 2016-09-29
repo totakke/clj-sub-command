e2b86eb3c299c1b09135a48156357205b95479a6
Toshiki Takeuchi <roimisia@gmail.com>
2016-09-29 14:41:03 +0900

    Migrate from midje to clojure.test

--- project.clj
+++ project.clj
@@ -4,8 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:plugins [[lein-midje "3.2"]]
-                   :dependencies [[midje "1.8.3"]]}
-             :1.7 [:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}]
+  :profiles {:1.7 [:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}]
              :1.6 [:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}]}
   :signing {:gpg-key "roimisia@gmail.com"})
---
+++ test/clj_sub_command/core_test.clj
@@ -0,0 +1,24 @@
+(ns clj-sub-command.core-test
+  (:require [clojure.test :refer :all]
+            [clj-sub-command.core :refer :all]))
+
+(deftest candidates-test
+  (let [candidates' #'clj-sub-command.core/candidates]
+    (is (= (candidates' "sttatus" #{"status" "commit" "push"}) ["status"]))
+    (is (= (candidates' "unknown" #{"status" "commit" "push"}) []))))
+
+(deftest sub-command-test
+  (let [[opts cmd args _ cands]
+        (sub-command ["-p" "8080" "--no-verbose" "command1" "filename"]
+                     :options  [["-p" "--port" :parse-fn #(Integer. %)]
+                                ["--[no-]verbose" :default true]]
+                     :commands [["command1" "desc for command1"]
+                                ["command2" "desc for command2"]])]
+    (testing "for option parsing"
+      (is (= opts {:port 8080, :verbose false})))
+    (testing "for command parsing"
+      (is (= cmd :command1)))
+    (testing "for sub-command arguments"
+      (is (= args ["filename"])))
+    (testing "for candidates"
+      (is (= cands ["command1" "command2"])))))
