f6e9614285a0874253229b604a2b9aaeeafaeb28
Toshiki Takeuchi <roimisia@gmail.com>
2018-01-05 19:27:39 +0900

    Add tools.cli

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/totakke/clj-sub-command"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]]
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
+                 [org.clojure/tools.cli "0.3.5"]]
   :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
