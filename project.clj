0f9cadd8a71021e611d4e7ea0ded3ed784a5c6b8
Toshiki Takeuchi <roimisia@gmail.com>
2018-01-05 17:58:38 +0900

    Refine clojure deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/totakke/clj-sub-command"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha13"]]}
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]]
+  :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
