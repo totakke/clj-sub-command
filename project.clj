c1e7f1a053ef950d634d40fffd1c58a823e305ac
Toshiki Takeuchi <roimisia@gmail.com>
2018-09-25 15:51:56 +0900

    Update tools.cli to 0.4.1

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/tools.cli "0.3.7"]]
+                 [org.clojure/tools.cli "0.4.1"]]
   :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}})
