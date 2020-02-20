c52e89f8b097fd3d596cb9d4962162afd737bad8
Toshiki Takeuchi <roimisia@gmail.com>
2020-02-20 19:24:46 +0900

    Update main clojure version to 1.10.1

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/totakke/clj-sub-command"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [org.clojure/clojurescript "1.10.339" :scope "provided"]
                  [org.clojure/tools.cli "0.4.1"]]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-doo "0.1.10"]]
-  :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
-             :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}}
+  :profiles {:1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}}
   :cljsbuild {:builds [{:id "test"
                         :source-paths ["src" "test"]
                         :compiler {:output-to "target/testable.js"
